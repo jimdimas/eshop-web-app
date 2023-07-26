45f2683e7aab0200a7c2f0b5adb2eb36064d835e
jimdimas <dimdimas@hotmail.gr>
2023-07-26 21:08:32 +0300

    added put,delete,get by id functionality

--- src/main/java/com/jimdimas/api/user/UserController.java
+++ src/main/java/com/jimdimas/api/user/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping({"/api/v1/user"})
@@ -17,8 +18,17 @@ public class UserController {
         return userService.getUsers();
     }
 
+    @GetMapping(path="{userId}")
+    public Optional<User> getUserById(@PathVariable Integer userId){ return userService.getUserById(userId); }
+
     @PostMapping
     public void addUser(@RequestBody User user){
         userService.addUser(user);
     }
+
+    @DeleteMapping(path="{userId}")
+    public void deleteUser(@PathVariable Integer userId){ userService.deleteUser(userId); }
+
+    @PutMapping(path="{userId}")
+    public void updateUser(@PathVariable Integer userId,@RequestBody User user){ userService.updateUser(userId,user); }
 }
--- src/main/java/com/jimdimas/api/user/UserService.java
+++ src/main/java/com/jimdimas/api/user/UserService.java
@@ -2,8 +2,10 @@ package com.jimdimas.api.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +21,9 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    @GetMapping
+    public Optional<User> getUserById(Integer userId) { return userRepository.findById(userId); }
+
     @PostMapping
     public void addUser(User user){
         Optional<User> userExists = userRepository.findUserByEmail(user.getEmail());
@@ -27,5 +32,49 @@ public class UserService {
         }
         user.setRole(Role.USER);
         userRepository.save(user);
+    }
+
+    @DeleteMapping
+    public void deleteUser(Integer userId){
+        Optional<User> userExists = userRepository.findById(userId);
+        if (!userExists.isPresent()){
+            throw new IllegalStateException("User with id : "+userId.toString()+" does not exist");
+        }
+        userRepository.deleteById(userId);
+    }
+
+    @PutMapping
+    public void updateUser(Integer userId,User user){
+        Optional<User> userExists = userRepository.findById(userId);
+        if (!userExists.isPresent()){
+            throw new IllegalStateException("User with id "+userId.toString()+" does not exist");
+        }
+        User updatedUser = userExists.get();
+
+        if (user.getEmail()!=null){
+            Optional<User> userEmailTaken = userRepository.findUserByEmail(user.getEmail());
+            if (userEmailTaken.isPresent()){
+                throw new IllegalStateException("Cannot update email to "+user.getEmail()+" , it's already taken.");
+            }
+            updatedUser.setEmail(user.getEmail());
+        }
+
+        if (user.getDob()!=null){
+            updatedUser.setDob(user.getDob());
+        }
+
+        if (user.getFirstName()!=null){
+            updatedUser.setFirstName(user.getFirstName());
+        }
+
+        if (user.getLastName()!=null){
+            updatedUser.setLastName(user.getLastName());
+        }
+
+        if (user.getPassword()!=null) {
+            updatedUser.setPassword(user.getPassword());
+        }
+
+        userRepository.save(updatedUser);
     }
 }
