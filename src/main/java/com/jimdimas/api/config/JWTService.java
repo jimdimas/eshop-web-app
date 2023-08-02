f9eafda9fcb722e643426c98e79f38c03d619fe3
jimdimas <jimdimas6@gmail.com>
2023-08-02 15:27:46 +0300

    altered JWTService implementation,used auth0 for jwt

--- src/main/java/com/jimdimas/api/config/JWTService.java
+++ src/main/java/com/jimdimas/api/config/JWTService.java
@@ -1,16 +1,25 @@
 package com.jimdimas.api.config;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
+import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,47 +27,43 @@ import java.util.function.Function;
 
 @Service
 public class JWTService {
+    @Value("${spring.properties.jwt.private_key}")
+    private String actualPrivateKey;
+    @Value("${spring.properties.jwt.public_key}")
+    private String actualPublicKey;
+    //The following keys are RSA keys objects , we read the actual values and then convert them to keys
+    private RSAPrivateKey privateKey;
+    private RSAPublicKey publicKey;
 
-    @Value("${spring.properties.jwt.secret_key}")   //get key from application.properties file
-    private String secretKey;
-
-    private Claims extractAllClaims(String token){
-        return Jwts.
-                parserBuilder().
-                setSigningKey(getSigningKey()).
-                build().
-                parseClaimsJwt(token).
-                getBody();
+    public String extractSubject(String token) throws JWTVerificationException{
+        DecodedJWT decodedJWT = this.decodeToken(token);
+        return decodedJWT.getSubject();
     }
 
-    private <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
-        Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
-    }
-    public String generateToken(Map<String,Object> claims, UserDetails userDetails){
-        Integer twoHoursMillis=1000*60*2;
-        return Jwts
-                .builder()
-                .setSubject(userDetails.getUsername())
-                .setClaims(claims)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+twoHoursMillis))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
-                .compact();
+    public String generateToken(UserDetails userDetails) throws JWTCreationException {
+        Integer twoHours = 1000 * 60 * 60 * 2;
+        Algorithm algorithm = Algorithm.RSA256(this.publicKey, this.privateKey);
+        return JWT.create()
+                .withIssuer("auth0")
+                .withSubject(userDetails.getUsername())
+                .withIssuedAt(new Date(System.currentTimeMillis()))
+                .withExpiresAt(new Date(System.currentTimeMillis() + twoHours))
+                .sign(algorithm);
 
     }
 
-    public String generateToken(UserDetails userDetails){   //used when we don't provide any extra claims apart from user email
-        return generateToken(new HashMap<String,Object>(),userDetails);
+    public Boolean verifyToken(String token) throws JWTVerificationException {
+        DecodedJWT decodedJWT = this.decodeToken(token);    //token validity is checked in decodeToken,if its not valid exception is thrown
+        return decodedJWT.getExpiresAt().before(new Date(System.currentTimeMillis()));
     }
 
-    private boolean isValidToken(String token,UserDetails userDetails){ //checks if token has not expired and if it's issued to the sender
-        return (extractClaim(token,Claims::getSubject).equals(userDetails.getUsername()) &&
-                extractClaim(token,Claims::getExpiration).before(new Date()));
-    }
-
-    private Key getSigningKey() {
-        byte[] keyInBytes = Decoders.BASE64.decode(this.secretKey);
-        return Keys.hmacShaKeyFor(keyInBytes);
+    private DecodedJWT decodeToken(String token) throws JWTVerificationException{
+        Algorithm algorithm = Algorithm.RSA256(this.publicKey, this.privateKey);
+        DecodedJWT decodedJWT;
+        JWTVerifier jwtVerifier = JWT
+                .require(algorithm)
+                .withIssuer("auth0")
+                .build();
+        return jwtVerifier.verify(token);
     }
 }
