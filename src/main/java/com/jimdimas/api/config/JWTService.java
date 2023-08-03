ac5dfc01648e6f512e43c7a329fd5572b81285ea
jimdimas <jimdimas6@gmail.com>
2023-08-03 06:03:40 +0300

    created UtilService class for string key to rsa key conversion and used it in JWTService

--- src/main/java/com/jimdimas/api/config/JWTService.java
+++ src/main/java/com/jimdimas/api/config/JWTService.java
@@ -6,20 +6,25 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.jimdimas.api.util.UtilService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,13 +32,22 @@ import java.util.function.Function;
 
 @Service
 public class JWTService {
-    @Value("${spring.properties.jwt.private_key}")
-    private String actualPrivateKey;
-    @Value("${spring.properties.jwt.public_key}")
-    private String actualPublicKey;
-    //The following keys are RSA keys objects , we read the actual values and then convert them to keys
     private RSAPrivateKey privateKey;
     private RSAPublicKey publicKey;
+    private final UtilService utilService;
+
+    public JWTService(@Value("${spring.properties.jwt.private_key}") String stringPrivateKey,
+                      @Value("${spring.properties.jwt.public_key}") String stringPublicKey){
+        utilService=new UtilService();
+        try {
+            publicKey=utilService.convertToRSAPublicKey(stringPublicKey);
+            privateKey=utilService.convertToRSAPrivateKey(stringPrivateKey);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public String extractSubject(String token) throws JWTVerificationException{
         DecodedJWT decodedJWT = this.decodeToken(token);
---
+++ src/main/java/com/jimdimas/api/util/UtilService.java
@@ -0,0 +1,46 @@
+package com.jimdimas.api.util;
+
+import java.util.Base64;
+import org.springframework.stereotype.Service;
+
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+@Service
+public class UtilService {
+
+    public RSAPublicKey convertToRSAPublicKey(String publicKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        if (!publicKey.contains("-----BEGIN PUBLIC KEY-----") || !publicKey.contains("-----END PUBLIC KEY-----"))
+        {
+            throw new InvalidKeySpecException("Wrong RSA key format");
+        }
+        publicKey = publicKey
+                .replace("-----BEGIN PUBLIC KEY-----","")
+                .replace("\n","")
+                .replace("-----END PUBLIC KEY-----","")
+                .replace(" ","");
+        byte[] publicKeyToBytes = Base64.getDecoder().decode(publicKey);
+        return (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(publicKeyToBytes));
+    }
+
+    public RSAPrivateKey convertToRSAPrivateKey(String privateKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        if (!privateKey.contains("-----BEGIN PRIVATE KEY-----") || !privateKey.contains("-----END PRIVATE KEY-----"))
+        {
+            throw new InvalidKeySpecException("Wrong RSA key format");
+        }
+        privateKey = privateKey
+                .replace("-----BEGIN PRIVATE KEY-----","")
+                .replace("\n","")
+                .replace("-----END PRIVATE KEY-----","")
+                .replace(" ","");
+        byte[] privateKeyToBytes = java.util.Base64.getDecoder().decode(privateKey);
+        return (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(privateKeyToBytes));
+    }
+
+}
